55a42421c132527a54a56fd80956a1bfe043cefc
abelnkuna7-cloud <abelnkuna7@gmail.com>
2026-08-28 15:47:05 +0200

    wire GA4 page tracking into router

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,6 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
+import { initAnalytics, trackPageView } from "@/lib/analytics";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
@@ -11,6 +12,18 @@ export const getRouter = () => {
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
+
+  if (typeof window !== "undefined") {
+    initAnalytics();
+
+    const sendPageView = () => {
+      const location = router.state.location;
+      trackPageView(`${location.pathname}${location.searchStr}${location.hash}`, document.title);
+    };
+
+    sendPageView();
+    router.subscribe("onResolved", sendPageView);
+  }
 
   return router;
 };
